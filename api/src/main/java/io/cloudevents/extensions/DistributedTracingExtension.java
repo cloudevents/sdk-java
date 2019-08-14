4655bc89d941d23dca7b9dbe94faf1169a3d9962
Fabio José <fabiojose@gmail.com>
2019-08-14 13:42:39 -0300

    InMemory representation for distributed tracing extension
    Signed-off-by: Fabio José <fabiojose@gmail.com>

--- api/src/main/java/io/cloudevents/extensions/DistributedTracingExtension.java
+++ api/src/main/java/io/cloudevents/extensions/DistributedTracingExtension.java
@@ -1,6 +1,7 @@
 package io.cloudevents.extensions;
 
 import io.cloudevents.Extension;
+import io.cloudevents.v02.ExtensionFormat;
 
 public class DistributedTracingExtension implements Extension {
 
@@ -29,5 +30,33 @@ public class DistributedTracingExtension implements Extension {
                 "traceparent='" + traceparent + '\'' +
                 ", tracestate='" + tracestate + '\'' +
                 '}';
+    }
+    
+    /**
+     * The in-memory format for distributed tracing.
+     * <br/>
+     * Details <a href="https://github.com/cloudevents/spec/blob/v0.2/extensions/distributed-tracing.md#in-memory-formats">here</a>
+     * @author fabiojose
+     *
+     */
+    public static class InMemory implements ExtensionFormat {
+    	
+    	private static final String IN_MEMORY_KEY = "distributedTracing";
+    	
+    	private final Extension extension;
+    	public InMemory(DistributedTracingExtension extension) {
+    		this.extension = extension;
+    	}
+
+		@Override
+		public String getKey() {
+			return IN_MEMORY_KEY;
+		}
+
+		@Override
+		public Extension getExtension() {
+			return extension;
+		}
+    	
     }
 }
