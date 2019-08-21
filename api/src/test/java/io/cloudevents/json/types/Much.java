a12d29eaf6362a5417e55c01021890eecbac799e
Fabio José <fabiojose@gmail.com>
2019-08-21 14:03:21 -0300

    bean to use with test
    Signed-off-by: Fabio José <fabiojose@gmail.com>

---
+++ api/src/test/java/io/cloudevents/json/types/Much.java
@@ -0,0 +1,20 @@
+package io.cloudevents.json.types;
+
+/**
+ * 
+ * @author fabiojose
+ *
+ */
+public class Much {
+
+	private String wow;
+
+	public String getWow() {
+		return wow;
+	}
+
+	public void setWow(String wow) {
+		this.wow = wow;
+	}
+
+}
