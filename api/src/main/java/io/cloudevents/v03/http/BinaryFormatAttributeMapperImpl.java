d41b52822152ab0b2424da28e6734e3545b1ae1e
Fabio José <fabiojose@gmail.com>
2019-08-26 21:27:37 -0300

    License and code format
    Signed-off-by: Fabio José <fabiojose@gmail.com>

--- api/src/main/java/io/cloudevents/v03/http/BinaryFormatAttributeMapperImpl.java
+++ api/src/main/java/io/cloudevents/v03/http/BinaryFormatAttributeMapperImpl.java
@@ -1,3 +1,18 @@
+/**
+ * Copyright 2019 The CloudEvents Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package io.cloudevents.v03.http;
 
 import static java.util.stream.Collectors.toMap;
@@ -10,6 +25,7 @@ import java.util.Map.Entry;
 import java.util.concurrent.atomic.AtomicReference;
 
 import io.cloudevents.fun.BinaryFormatAttributeMapper;
+import io.cloudevents.v03.ContextAttributes;
 
 /**
  * 
@@ -41,11 +57,13 @@ public class BinaryFormatAttributeMapperImpl implements BinaryFormatAttributeMap
 				}
 			})
 			.filter(header -> header.getKey().startsWith(HEADER_PREFIX))
-			.map(header -> new SimpleEntry<>(header.getKey().substring(HEADER_PREFIX.length()), header.getValue()))
+			.map(header -> new SimpleEntry<>(header.getKey()
+					.substring(HEADER_PREFIX.length()), header.getValue()))
 			.map(header -> new SimpleEntry<>(header.getKey(), header.getValue().toString()))
 			.collect(toMap(Entry::getKey, Entry::getValue));
 		
-		result.put("datacontenttype", ct.get().getValue().toString());
+		result.put(ContextAttributes.datacontenttype.name(),
+				ct.get().getValue().toString());
 			
 		return result;
 	}
