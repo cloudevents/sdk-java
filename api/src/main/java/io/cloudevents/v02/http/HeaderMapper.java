480c7b5443bed9e0b21e780d518c65b30e077131
Fabio José <fabiojose@gmail.com>
2019-09-10 22:01:42 -0300

    Change the reference to functional interface header mapper
    Signed-off-by: Fabio José <fabiojose@gmail.com>

--- api/src/main/java/io/cloudevents/v02/http/HeaderMapper.java
+++ api/src/main/java/io/cloudevents/v02/http/HeaderMapper.java
@@ -25,7 +25,7 @@ import java.util.Objects;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
-import io.cloudevents.fun.BinaryFormatHeaderMapper;
+import io.cloudevents.fun.FormatHeaderMapper;
 import io.cloudevents.v02.ContextAttributes;
 
 /**
@@ -39,7 +39,7 @@ public final class HeaderMapper {
 	private static final String HTTP_CONTENT_TYPE = "Content-Type";
 
 	/**
-	 * Following the signature of {@link BinaryFormatHeaderMapper}
+	 * Following the signature of {@link FormatHeaderMapper}
 	 * @param attributes The map of attributes created by {@link AttributeMapper}
 	 * @param extensions The map of extensions created by {@link ExtensionMapper}
 	 * @return The map of HTTP Headers
