de58785f62965f2bb9afc1ed930c2f8305c2df2c
Fabio José <fabiojose@gmail.com>
2019-09-18 21:36:18 -0300

    Umarshaller for structured content mode
    Signed-off-by: Fabio José <fabiojose@gmail.com>

--- api/src/main/java/io/cloudevents/v02/http/Unmarshallers.java
+++ api/src/main/java/io/cloudevents/v02/http/Unmarshallers.java
@@ -2,10 +2,12 @@ package io.cloudevents.v02.http;
 
 import io.cloudevents.extensions.DistributedTracingExtension;
 import io.cloudevents.format.BinaryUnmarshaller;
+import io.cloudevents.format.StructuredUnmarshaller;
 import io.cloudevents.format.builder.HeadersStep;
 import io.cloudevents.json.Json;
 import io.cloudevents.v02.AttributesImpl;
 import io.cloudevents.v02.CloudEventBuilder;
+import io.cloudevents.v02.CloudEventImpl;
 
 /**
  * 
@@ -36,5 +38,39 @@ public class Unmarshallers {
 				.map(DistributedTracingExtension::unmarshall)
 				.next()
 				.builder(CloudEventBuilder.<T>builder()::build);
+	}
+	
+	/**
+	 * Builds a Structured Content Mode unmarshaller to unmarshal JSON as CloudEvents data
+	 * for HTTP Transport Binding
+	 * 
+	 * @param <T> The 'data' type
+	 * @param typeOfData The type reference to use for 'data' unmarshal
+	 * @return A step to supply the headers, payload and to unmarshal
+	 * @see StructuredUnmarshaller
+	 */
+	public static <T> HeadersStep<AttributesImpl, T, String> 
+			structured(Class<T> typeOfData) {
+		
+		return
+		StructuredUnmarshaller.<AttributesImpl, T, String>
+		  builder()
+			.map(ExtensionMapper::map)
+			.map(DistributedTracingExtension::unmarshall)
+			.next()
+			.map((payload, extensions) -> {			
+				CloudEventImpl<T> event =
+					Json.<CloudEventImpl<T>>
+						decodeValue(payload, CloudEventImpl.class, typeOfData);
+				
+				CloudEventBuilder<T> builder = 
+					CloudEventBuilder.<T>builder(event);
+				
+				extensions.get().forEach(extension -> {
+					builder.withExtension(extension);
+				});
+				
+				return builder.build();
+			});
 	}
 }
