8327a97b0491d88dc1b2c6bb369facec3afc815e
Fabio José <fabiojose@gmail.com>
2019-09-19 21:19:49 -0300

    Kafka Attributes mapper for spec 0.2
    Signed-off-by: Fabio José <fabiojose@gmail.com>

---
+++ kafka/src/test/java/io/cloudevents/v03/kafka/AttributeMapperTest.java
@@ -0,0 +1,85 @@
+package io.cloudevents.v03.kafka;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.junit.jupiter.api.Test;
+
+import io.cloudevents.v03.kafka.AttributeMapper;
+
+public class AttributeMapperTest {
+	@Test
+	public void error_when_headers_map_isnull() {
+		// act
+		assertThrows(NullPointerException.class, () -> {
+			AttributeMapper.map(null);
+		});
+	}
+	
+	@Test
+	public void error_when_header_value_is_not_byte_array() {
+		// setup
+		Map<String, Object> headers = new HashMap<>();
+		headers.put("ce_specversion", "0.3");
+
+		assertThrows(ClassCastException.class, () -> {
+			AttributeMapper.map(headers);
+		});
+	}
+	
+	@Test
+	public void should_map_cespecversion_to_specversion() {
+		// setup
+		Map<String, Object> headers = new HashMap<>();
+		headers.put("ce_specversion", "0.3".getBytes());
+		
+		String expected = "specversion";
+		
+		// act
+		Map<String, String> attributes = 
+				AttributeMapper.map(headers);
+		
+		// assert
+		assertNotNull(attributes.get(expected));
+	}
+	
+	@Test
+	public void should_not_map_null_value() {
+		// setup
+		Map<String, Object> headers = new HashMap<>();
+		headers.put("ce_type", null);
+		
+		String expected = "type";
+		
+		// act
+		Map<String, String> attributes = 
+				AttributeMapper.map(headers);
+		
+		// assert
+		assertFalse(attributes.containsKey(expected));
+	}
+	
+	@Test
+	public void should_all_without_prefix_ce() {
+		// setup
+		Map<String, Object> myHeaders = new HashMap<>();
+		myHeaders.put("ce_id", "0x11".getBytes());
+		myHeaders.put("ce_source", "/source".getBytes());
+		myHeaders.put("ce_specversion", "0.3".getBytes());
+		myHeaders.put("ce_type", "br.my".getBytes());
+		myHeaders.put("ce_time", "2019-09-16T20:49:00Z".getBytes());
+		myHeaders.put("ce_schemaurl", "http://my.br".getBytes());
+		myHeaders.put("Content-Type", "application/json".getBytes());
+		
+		Map<String, String> actual = AttributeMapper.map(myHeaders);
+		
+		actual.keySet()
+			.forEach((attribute) -> {
+				assertFalse(attribute.startsWith("ce_"));
+			});
+	}
+}
