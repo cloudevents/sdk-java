76946e63f3681be04ae3796dd5706b63c477f1a4
Fabio José <fabiojose@gmail.com>
2019-09-28 11:18:59 -0300

    gpg batch sign: finally works
    Remove the test commands
    
    Remove the plugin args from pom.xml
    
    Signed-off-by: Fabio José <fabiojose@gmail.com>

--- .travis.deploy.sh
+++ .travis.deploy.sh
@@ -2,29 +2,14 @@ openssl aes-256-cbc -K $encrypted_3210c925a91b_key \
        -iv $encrypted_3210c925a91b_iv \
        -in .travis.secring.enc -out .travis.secring -d
 
-#gpg --keyring=$TRAVIS_BUILD_DIR/pubring.gpg \
-#     --no-default-keyring \
-#     --import .travis.pubring
-
-gpg2 --version
-
 gpg2 --import .travis.pubring
-
-#gpg --allow-secret-key-import --keyring=$TRAVIS_BUILD_DIR/secring.gpg \
-#     --no-default-keyring \
-#     --import .travis.secring
 
 gpg2 --batch --allow-secret-key-import --import .travis.secring
 
 export GPG_TTY=$(tty)
 
-#mvn clean deploy -P release -DskipTests \
-#    --settings .travis.settings.xml \
-#    -Dgpg.executable=gpg2 \
-#    -Dgpg.passphrase=$PASSPHRASE
-
-mvn package org.apache.maven.plugins:maven-gpg-plugin:1.6:sign -P release \
-    -DskipTests \
+mvn clean deploy -P release -DskipTests \
+    --settings .travis.settings.xml \
     -Dgpg.executable=gpg2 \
     -Dgpg.keyname=4F144A60ECA44F0D \
     -Dgpg.passphrase=$PASSPHRASE
