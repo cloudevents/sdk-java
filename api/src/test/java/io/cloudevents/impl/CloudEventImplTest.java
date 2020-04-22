924dbed6e79c568483e063bf24a86c68a7e18fe9
Francesco Guardiani <francescoguard@gmail.com>
2020-04-22 17:05:04 +0200

    Test for implementation of equals (#126)
    * Test for equals
    
    Signed-off-by: Francesco Guardiani <francescoguard@gmail.com>
    
    * Maven fix
    
    Signed-off-by: Francesco Guardiani <francescoguard@gmail.com>
    
    * Funny pom issues
    
    Signed-off-by: Francesco Guardiani <francescoguard@gmail.com>
    
    * Fixed pom issues
    
    Signed-off-by: Francesco Guardiani <francescoguard@gmail.com>

--- api/src/test/java/io/cloudevents/impl/CloudEventImplTest.java
+++ api/src/test/java/io/cloudevents/impl/CloudEventImplTest.java
@@ -1,4 +1,57 @@
 package io.cloudevents.impl;
 
-public class CloudEventImplTest  {
+import io.cloudevents.CloudEvent;
+import org.junit.jupiter.api.Test;
+
+import static io.cloudevents.test.Data.*;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class CloudEventImplTest {
+
+    @Test
+    public void testEqualityV03() {
+        CloudEvent event1 = CloudEvent.buildV03()
+            .withId(ID)
+            .withType(TYPE)
+            .withSource(SOURCE)
+            .withData(DATACONTENTTYPE_JSON, DATASCHEMA, DATA_JSON_SERIALIZED)
+            .withSubject(SUBJECT)
+            .withTime(TIME)
+            .build();
+
+        CloudEvent event2 = CloudEvent.buildV03()
+            .withId(ID)
+            .withType(TYPE)
+            .withSource(SOURCE)
+            .withData(DATACONTENTTYPE_JSON, DATASCHEMA, DATA_JSON_SERIALIZED)
+            .withSubject(SUBJECT)
+            .withTime(TIME)
+            .build();
+
+        assertThat(event1).isEqualTo(event2);
+    }
+
+    @Test
+    public void testEqualityV1() {
+        CloudEvent event1 = CloudEvent.buildV1()
+            .withId(ID)
+            .withType(TYPE)
+            .withSource(SOURCE)
+            .withData(DATACONTENTTYPE_JSON, DATASCHEMA, DATA_JSON_SERIALIZED)
+            .withSubject(SUBJECT)
+            .withTime(TIME)
+            .build();
+
+        CloudEvent event2 = CloudEvent.buildV1()
+            .withId(ID)
+            .withType(TYPE)
+            .withSource(SOURCE)
+            .withData(DATACONTENTTYPE_JSON, DATASCHEMA, DATA_JSON_SERIALIZED)
+            .withSubject(SUBJECT)
+            .withTime(TIME)
+            .build();
+
+        assertThat(event1).isEqualTo(event2);
+    }
+
 }
