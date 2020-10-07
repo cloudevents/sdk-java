6d81bc435465432dc586a3adc82b369fa66982bb
slinkydeveloper <francescoguard@gmail.com>
2020-10-07 15:57:12 +0200

    Javadocs
    Signed-off-by: Francesco Guardiani <francescoguard@gmail.com>

--- api/src/main/java/io/cloudevents/CloudEventData.java
+++ api/src/main/java/io/cloudevents/CloudEventData.java
@@ -2,10 +2,19 @@ package io.cloudevents;
 
 import java.util.function.Function;
 
+/**
+ * Interface that defines a container for CloudEvent data.
+ *
+ * Every CloudEvent data has
+ */
 public interface CloudEventData {
 
+    /**
+     * @return this CloudEventData, represented as bytes. Note: this operation may be expensive, depending on the internal representation of data
+     */
     byte[] toBytes();
 
+    //TBD
     <T> T to(Function<Object, T> mapper);
 
 }
