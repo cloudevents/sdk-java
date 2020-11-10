c59cfb6b8d0b9c152f37bdb2b3e09a1aa69d85b3
slinkydeveloper <francescoguard@gmail.com>
2020-11-10 13:43:57 +0100

    Test
    Signed-off-by: Francesco Guardiani <francescoguard@gmail.com>

---
+++ core/src/test/java/io/cloudevents/core/impl/CloudEventUtilsTest.java
@@ -0,0 +1,59 @@
+package io.cloudevents.core.impl;
+
+import io.cloudevents.CloudEvent;
+import io.cloudevents.CloudEventData;
+import io.cloudevents.core.builder.CloudEventBuilder;
+import io.cloudevents.core.mock.MyCloudEventData;
+import io.cloudevents.rw.CloudEventDataMapper;
+import org.junit.jupiter.api.Test;
+
+import java.net.URI;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class CloudEventUtilsTest {
+
+    @Test
+    void mapDataWithNullData() {
+        AtomicInteger i = new AtomicInteger();
+        CloudEventDataMapper<CloudEventData> mapper = data -> {
+            i.incrementAndGet();
+            return data;
+        };
+
+        CloudEvent cloudEvent = CloudEventBuilder.v1()
+            .withId("aaa")
+            .withSource(URI.create("localhost"))
+            .withType("bbb")
+            .build();
+
+        assertThat(CloudEventUtils.mapData(cloudEvent, mapper))
+            .isNull();
+        assertThat(i)
+            .hasValue(0);
+    }
+
+    @Test
+    void mapData() {
+        AtomicInteger i = new AtomicInteger();
+        CloudEventDataMapper<CloudEventData> mapper = data -> {
+            i.incrementAndGet();
+            return data;
+        };
+
+        MyCloudEventData data = new MyCloudEventData(10);
+
+        CloudEvent cloudEvent = CloudEventBuilder.v1()
+            .withId("aaa")
+            .withSource(URI.create("localhost"))
+            .withType("bbb")
+            .withData(data)
+            .build();
+
+        assertThat(CloudEventUtils.mapData(cloudEvent, mapper))
+            .isEqualTo(data);
+        assertThat(i)
+            .hasValue(1);
+    }
+}
