5099b31f6cdb4866de659ed6a313d23d6c468a4f
Dave Syer <dsyer@vmware.com>
2020-11-30 10:27:44 +0000

    HTTP converters for CloudEvent in Spring (#312)
    Supports MVC and WebFlux (blocking and non-blocking) HTTP.
    User can work with `CloudEvent` as a `POJO` type and inject it
    into `@ReqestMapping` methods.
    
    Signed-off-by: Dave Syer <dsyer@vmware.com>
    
    Co-authored-by: Oleg Zhurakousky <ozhurakousky@pivotal.io>

---
+++ spring/src/main/java/io/cloudevents/spring/webflux/CloudEventHttpMessageReader.java
@@ -0,0 +1,77 @@
+/*
+ * Copyright 2019-2019 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.cloudevents.spring.webflux;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+
+import io.cloudevents.CloudEvent;
+import io.cloudevents.core.message.MessageReader;
+import io.cloudevents.spring.http.CloudEventHttpUtils;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import org.springframework.core.ResolvableType;
+import org.springframework.core.io.buffer.DataBufferUtils;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ReactiveHttpInputMessage;
+import org.springframework.http.codec.HttpMessageReader;
+import org.springframework.util.StreamUtils;
+
+/**
+ * A reactive {@link HttpMessageReader} for {@link CloudEvent CloudEvents}, converting
+ * from an HTTP request to a cloud event. Supports the use of {@link CloudEvent} as an
+ * input to a reactive endpoint.
+ * 
+ * @author Dave Syer
+ *
+ */
+public class CloudEventHttpMessageReader implements HttpMessageReader<CloudEvent> {
+
+	@Override
+	public List<MediaType> getReadableMediaTypes() {
+		return Arrays.asList(MediaType.APPLICATION_OCTET_STREAM, MediaType.ALL);
+	}
+
+	@Override
+	public boolean canRead(ResolvableType elementType, MediaType mediaType) {
+		return CloudEvent.class.isAssignableFrom(elementType.toClass());
+	}
+
+	@Override
+	public Flux<CloudEvent> read(ResolvableType elementType, ReactiveHttpInputMessage message,
+			Map<String, Object> hints) {
+		return Flux.from(readMono(elementType, message, hints));
+	}
+
+	@Override
+	public Mono<CloudEvent> readMono(ResolvableType elementType, ReactiveHttpInputMessage message,
+			Map<String, Object> hints) {
+		HttpHeaders headers = message.getHeaders();
+		Mono<byte[]> body = DataBufferUtils.join(message.getBody()).map(buffer -> {
+			try {
+				return StreamUtils.copyToByteArray(buffer.asInputStream(true));
+			}
+			catch (Exception e) {
+				throw new IllegalArgumentException(e);
+			}
+		});
+		return body.map(bytes -> CloudEventHttpUtils.toReader(headers, () -> bytes)).map(MessageReader::toEvent);
+	}
+
+}
---
+++ spring/src/main/java/io/cloudevents/spring/webflux/CloudEventHttpMessageWriter.java
@@ -0,0 +1,125 @@
+/*
+ * Copyright 2019-2019 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.cloudevents.spring.webflux;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+
+import io.cloudevents.CloudEvent;
+import io.cloudevents.CloudEventData;
+import io.cloudevents.SpecVersion;
+import io.cloudevents.core.CloudEventUtils;
+import io.cloudevents.core.format.EventFormat;
+import io.cloudevents.core.message.MessageWriter;
+import io.cloudevents.rw.CloudEventRWException;
+import io.cloudevents.rw.CloudEventWriter;
+import io.cloudevents.spring.http.CloudEventsHeaders;
+import org.reactivestreams.Publisher;
+import reactor.core.publisher.Mono;
+
+import org.springframework.core.ResolvableType;
+import org.springframework.core.io.buffer.DataBuffer;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ReactiveHttpOutputMessage;
+import org.springframework.http.codec.HttpMessageWriter;
+
+/**
+ * A reactive {@link HttpMessageWriter} for {@link CloudEvent CloudEvents}, converting
+ * from a cloud event to an HTTP response. Supports the use of {@link CloudEvent} as an
+ * output from a reactive endpoint.
+ * 
+ * @author Dave Syer
+ *
+ */
+public class CloudEventHttpMessageWriter implements HttpMessageWriter<CloudEvent> {
+
+	@Override
+	public List<MediaType> getWritableMediaTypes() {
+		return Arrays.asList(MediaType.APPLICATION_OCTET_STREAM, MediaType.ALL);
+	}
+
+	@Override
+	public boolean canWrite(ResolvableType elementType, MediaType mediaType) {
+		return CloudEvent.class.isAssignableFrom(elementType.toClass());
+	}
+
+	@Override
+	public Mono<Void> write(Publisher<? extends CloudEvent> inputStream, ResolvableType elementType,
+			MediaType mediaType, ReactiveHttpOutputMessage message, Map<String, Object> hints) {
+		return Mono.from(inputStream).map(CloudEventUtils::toReader)
+				.flatMap(reader -> reader.read(new ReactiveHttpMessageWriter(message)));
+	}
+
+	private static class ReactiveHttpMessageWriter
+			implements MessageWriter<CloudEventWriter<Mono<Void>>, Mono<Void>>, CloudEventWriter<Mono<Void>> {
+
+		private final ReactiveHttpOutputMessage response;
+
+		public ReactiveHttpMessageWriter(ReactiveHttpOutputMessage response) {
+			this.response = response;
+		}
+
+		// Binary visitor factory
+
+		@Override
+		public CloudEventWriter<Mono<Void>> create(SpecVersion version) {
+			this.response.getHeaders().set(CloudEventsHeaders.SPEC_VERSION, version.toString());
+			return this;
+		}
+
+		// Binary visitor
+
+		@Override
+		public ReactiveHttpMessageWriter withAttribute(String name, String value) throws CloudEventRWException {
+			this.response.getHeaders().set(CloudEventsHeaders.ATTRIBUTES_TO_HEADERS.get(name), value);
+			return this;
+		}
+
+		@Override
+		public ReactiveHttpMessageWriter withExtension(String name, String value) throws CloudEventRWException {
+			this.response.getHeaders().set("ce-" + name, value);
+			return this;
+		}
+
+		@Override
+		public Mono<Void> end(CloudEventData value) throws CloudEventRWException {
+			return copy(value.toBytes(), this.response);
+		}
+
+		@Override
+		public Mono<Void> end() {
+			return copy(new byte[0], this.response);
+		}
+
+		// Structured visitor
+
+		@Override
+		public Mono<Void> setEvent(EventFormat format, byte[] value) throws CloudEventRWException {
+			this.response.getHeaders().set(HttpHeaders.CONTENT_TYPE, format.serializedContentType());
+			return copy(value, this.response);
+		}
+
+		private Mono<Void> copy(byte[] bytes, ReactiveHttpOutputMessage message) {
+			DataBuffer data = message.bufferFactory().wrap(bytes);
+			message.getHeaders().setContentLength(bytes.length);
+			return message.writeWith(Mono.just(data));
+		}
+
+	}
+
+}
