01c3cdfeff14d74c5fcb801856f275a904ef3720
Day, Jeremy(jday) <jday@paypal.com>
2021-04-13 10:43:16 -0700

    Address Review Comments
    Signed-off-by: Day, Jeremy(jday) <jday@paypal.com>

--- formats/protobuf/src/main/java/io/cloudevents/protobuf/ProtoCloudEventData.java
+++ formats/protobuf/src/main/java/io/cloudevents/protobuf/ProtoCloudEventData.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2018-Present The CloudEvents Authors
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
 package io.cloudevents.protobuf;
 
 import com.google.protobuf.Message;
@@ -16,7 +32,7 @@ public interface ProtoCloudEventData extends CloudEventData {
     Message getMessage();
 
     /**
-     * Convienience helper to wrap a Protobuff Message as
+     * Convienience helper to wrap a Protobuf Message as
      * CloudEventData.
      *
      * @param protoMessage
