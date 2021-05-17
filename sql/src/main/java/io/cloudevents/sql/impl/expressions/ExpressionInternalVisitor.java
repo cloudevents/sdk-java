38f431e60820ed41ef915a014beae4603cdff6e7
slinkydeveloper <francescoguard@gmail.com>
2021-05-17 17:46:14 +0200

    Little mistake
    Signed-off-by: Francesco Guardiani <francescoguard@gmail.com>

--- sql/src/main/java/io/cloudevents/sql/impl/expressions/ExpressionInternalVisitor.java
+++ sql/src/main/java/io/cloudevents/sql/impl/expressions/ExpressionInternalVisitor.java
@@ -8,47 +8,31 @@ public interface ExpressionInternalVisitor<T> {
         return visitExpressionInternal(accessAttributeExpression);
     }
 
-    ;
-
     default T visitBaseBinaryExpression(BaseBinaryExpression baseBinaryExpression) {
         return visitExpressionInternal(baseBinaryExpression);
     }
-
-    ;
 
     default T visitExistsExpression(ExistsExpression existsExpression) {
         return visitExpressionInternal(existsExpression);
     }
 
-    ;
-
     default T visitFunctionInvocationExpression(FunctionInvocationExpression functionInvocationExpression) {
         return visitExpressionInternal(functionInvocationExpression);
     }
-
-    ;
 
     default T visitInExpression(InExpression inExpression) {
         return visitExpressionInternal(inExpression);
     }
 
-    ;
-
     default T visitLikeExpression(LikeExpression likeExpression) {
         return visitExpressionInternal(likeExpression);
     }
-
-    ;
 
     default T visitBaseUnaryExpression(BaseUnaryExpression baseUnaryExpression) {
         return visitExpressionInternal(baseUnaryExpression);
     }
 
-    ;
-
     default T visitValueExpression(ValueExpression valueExpression) {
         return visitExpressionInternal(valueExpression);
     }
-
-    ;
 }
