d64aff7327ba3b2d102ad658af009ab76f925a77
mxsm <ljbmxsm@gmail.com>
2023-02-21 01:06:28 +0800

    [#521] Remove unused imports (#522)
    Signed-off-by: mxsm <ljbmxsm@gmail.com>

--- examples/restful-ws-quarkus/src/test/java/io/cloudevents/examples/quarkus/UserResourceTest.java
+++ examples/restful-ws-quarkus/src/test/java/io/cloudevents/examples/quarkus/UserResourceTest.java
@@ -4,7 +4,6 @@ import io.quarkus.test.junit.QuarkusTest;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
-import static org.hamcrest.CoreMatchers.is;
 
 @QuarkusTest
 public class UserResourceTest {
--- http/restful-ws-integration-tests/restful-ws-spring/src/test/java/io/cloudevents/http/restful/ws/spring/JerseyConfig.java
+++ http/restful-ws-integration-tests/restful-ws-spring/src/test/java/io/cloudevents/http/restful/ws/spring/JerseyConfig.java
@@ -22,8 +22,6 @@ import io.cloudevents.http.restful.ws.TestResource;
 import org.glassfish.jersey.server.ResourceConfig;
 import org.springframework.context.annotation.Configuration;
 
-import javax.ws.rs.ApplicationPath;
-
 @Configuration
 public class JerseyConfig extends ResourceConfig {
     public JerseyConfig() {
