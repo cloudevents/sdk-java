7d48b26be27e1a1d972060392391bdb672e13316
Jem Day <Jem.Day@cliffhanger.com>
2023-02-22 14:37:03 -0800

    Fixed issue where mutiple serialize/de-serialize operations would result in corrupted data if the data was a protobuf message object.
    Introduced equality checks for ProtoDataWrapper.
    
    Refactored and cleaned up data-wrappers.
    
    Signed-off-by: Jem Day <Jem.Day@cliffhanger.com>

--- formats/protobuf/src/main/java/io/cloudevents/protobuf/ProtoDataWrapper.java
+++ formats/protobuf/src/main/java/io/cloudevents/protobuf/ProtoDataWrapper.java
@@ -16,7 +16,10 @@
  */
 package io.cloudevents.protobuf;
 
+import com.google.protobuf.Any;
 import com.google.protobuf.Message;
+
+import java.util.Arrays;
 
 class ProtoDataWrapper implements ProtoCloudEventData {
 
@@ -34,5 +37,50 @@ class ProtoDataWrapper implements ProtoCloudEventData {
     @Override
     public byte[] toBytes() {
         return protoMessage.toByteArray();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+
+        if (this == obj) {
+            return (true);
+        }
+
+        if (!(obj instanceof ProtoDataWrapper)) {
+            return (false);
+        }
+
+        // Now compare the actual data
+        ProtoDataWrapper rhs = (ProtoDataWrapper) obj;
+
+        if (this.getMessage() == rhs.getMessage()){
+            return true;
+        }
+
+        // This is split out for readability.
+        //  Compare the content in terms onf an 'Any'.
+        //  - Verify the types match
+        //  - Verify the values match.
+
+        final Any lhsAny = getAsAny(this.getMessage());
+        final Any rhsAny = getAsAny(rhs.getMessage());
+
+        final boolean typesMatch = (ProtoSupport.extractMessageType(lhsAny).equals(ProtoSupport.extractMessageType(rhsAny)));
+
+        if (typesMatch) {
+            return lhsAny.getValue().equals(rhsAny.getValue());
+        } else {
+            return false;
+        }
+    }
+
+    private Any getAsAny(Message m) {
+
+        if (m instanceof Any) {
+            return (Any) m;
+        }
+
+        return Any.pack(m);
+
     }
 }
