3399747e9e99ca5929e57ae7d7de521e171433ae
Robert Pospisil <42009775+robp94@users.noreply.github.com>
2023-05-10 15:00:08 +0200

    Support westful-ws jakarta ee9 namespace (#469)
    * Replace javax restful-ws namespace usage with jakarata in own module
    * Add microprofile with openliberty example using new jar
    * Update gitignore to cover all copied directories
    * Update Jersey Version to support jakarta.* namespace packages
    * Port jersey integration tests from javax.* to jakarta.*
    * Undo changes to existing integration test package names
    * Add Integration test for Microprofile/Liberty for Jee9 package
    * Add documentation for new Jakarta package
    
    Signed-off-by: Robert Pospisil <robert.pospisil@mobilexag.de>
    Co-authored-by: Alex Butcher <21243172+abutch3r@users.noreply.github.com>

---
+++ examples/restful-ws-microprofile-liberty/src/main/java/io/cloudevents/examples/microprofile/CloudEventsJaxrsService.java
@@ -0,0 +1,56 @@
+package io.cloudevents.examples.microprofile;
+
+import io.cloudevents.CloudEvent;
+import io.cloudevents.core.builder.CloudEventBuilder;
+import jakarta.enterprise.context.RequestScoped;
+import jakarta.ws.rs.*;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+
+import java.net.URI;
+import java.nio.charset.StandardCharsets;
+
+@RequestScoped
+@Path("events")
+public class CloudEventsJaxrsService {
+
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public CloudEvent retrieveEvent(){
+        System.out.println("Received request for an event");
+        return CloudEventBuilder.v1()
+                .withData("{\"message\":\"Welcome to this Cloudevents + Microprofile example\"}".getBytes())
+                .withDataContentType("application/json")
+                .withId("hello")
+                .withType("example.http")
+                .withSource(URI.create("http://localhost"))
+                .build();
+    }
+
+    //Example Curl - curl -v http://localhost:9080/cloudevents-restful-ws-microprofile-example/events -H "Ce-Specversion: 1.0" -H "Ce-Type: User" -H "Ce-Source: io.cloudevents.examples/user" -H "Ce-Id: 536808d3-88be-4077-9d7a-a3f162705f78" -H "Content-Type: application/json" -H "Ce-Subject: SUBJ-0001" -d "hello"
+
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response postEvent(CloudEvent event){
+        System.out.println("Received request providing a event with body "+ new String(event.getData().toBytes(), StandardCharsets.UTF_8));
+        System.out.println(event);
+        return Response.noContent().build();
+    }
+
+    //Example Curl - curl -v http://localhost:9080/cloudevents-restful-ws-microprofile-example/events/echo -H "Ce-Specversion: 1.0" -H "Ce-Type: User" -H "Ce-Source: io.cloudevents.examples/user" -H "Ce-Id: 536808d3-88be-4077-9d7a-a3f162705f78" -H "Content-Type: application/json" -H "Ce-Subject: SUBJ-0001" -d "hello"
+
+    @POST
+    @Path("/echo")
+    @Consumes(MediaType.APPLICATION_JSON)
+    public CloudEvent echo(CloudEvent event){
+        return CloudEventBuilder.v1()
+                .withData("application/json", ("{\"echo\": \"" + new String(event.getData().toBytes(),StandardCharsets.UTF_8) + "\"}").getBytes())
+                .withId("echo")
+                .withType("echo.http")
+                .withSource(URI.create("http://localhost"))
+                .build();
+    }
+
+
+}
