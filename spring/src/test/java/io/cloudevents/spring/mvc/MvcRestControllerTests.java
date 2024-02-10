bdc6faf17378732d566f1526f7d048f4e1300fb5
Michele <lars.michele@tu-dortmund.de>
2024-02-10 18:21:34 +0100

    chores: upgrade spring dependencies to spring boot 3.1.8
    Signed-off-by: Michele <lars.michele@tu-dortmund.de>

--- spring/src/test/java/io/cloudevents/spring/mvc/MvcRestControllerTests.java
+++ spring/src/test/java/io/cloudevents/spring/mvc/MvcRestControllerTests.java
@@ -29,7 +29,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
