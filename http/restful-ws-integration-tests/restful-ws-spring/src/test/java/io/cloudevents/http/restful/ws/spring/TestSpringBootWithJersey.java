1bf4e412919143f15fd6579cd09f83b9c7854b98
Jacek Bilski <jacek@bilski.tech>
2026-05-19 18:01:22 +0200

    Upgrading Spring Boot to the newest 2.x version, used "Migrate to Spring Boot 2.7" OpenRewrite recipe.
    Signed-off-by: Jacek Bilski <jacek@bilski.tech>

--- http/restful-ws-integration-tests/restful-ws-spring/src/test/java/io/cloudevents/http/restful/ws/spring/TestSpringBootWithJersey.java
+++ http/restful-ws-integration-tests/restful-ws-spring/src/test/java/io/cloudevents/http/restful/ws/spring/TestSpringBootWithJersey.java
@@ -24,16 +24,13 @@ import io.cloudevents.http.restful.ws.CloudEventsProvider;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.web.server.LocalServerPort;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.boot.test.web.server.LocalServerPort;
 
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.WebTarget;
 import java.net.URI;
 
-@ExtendWith(SpringExtension.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class TestSpringBootWithJersey extends BaseTest {
 
